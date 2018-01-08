c9dc888b2e7a5b277ced2fe6cc6af7ed07cb5508
Jacob Johnson <jj.89@protonmail.com>
2018-01-08 17:40:59 -0600

    added register.html and login.html

--- main.py
+++ main.py
@@ -6,17 +6,30 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = '7v5XIQL9aFad7d'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    posts = db.relationship('Posts', backref='owner')
+
+    def __init__(self, username, password)
+        self.username = username
+        self.password = password
 
 class Posts(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
     post = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-    def __init__(self, title, post):
+    def __init__(self, title, post, owner):
         self.title = title
         self.post = post
+        self.owner = owner
 
 
 @app.route('/blog', methods=['POST','GET'])
@@ -44,9 +57,6 @@ def add_post():
         blog_title = request.form['blog-title']
         blog_post = request.form['blog-post']
         new_post = Posts(blog_title, blog_post)
-        
-
-
 
         if len(blog_title) == 0:
             title_error = "Please enter a title"
