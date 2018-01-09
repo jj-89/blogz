f03ccfbdcb42521a1e5838b0ccf620e956acca50
Jacob Johnson <jj.89@protonmail.com>
2018-01-08 19:52:09 -0600

    Added register/login/logout functions

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ app.secret_key = '7v5XIQL9aFad7d'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120))
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     posts = db.relationship('Posts', backref='owner')
 
-    def __init__(self, username, password)
-        self.username = username
+    def __init__(self, email, password):
+        self.email = email
         self.password = password
 
 class Posts(db.Model):
@@ -31,24 +31,77 @@ class Posts(db.Model):
         self.post = post
         self.owner = owner
 
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+@app.route('/register', methods = ['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO - Validate registration
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/blog')
+        else:
+            flash("User already exists",'error')
+
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.password == password:
+            session['email'] = email
+            flash("Logged in")
+            return redirect('/blog')
+        else:
+            flash("Email and password do not match, or the user does not exist", 'error') 
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/login')
 
 @app.route('/blog', methods=['POST','GET'])
 def index():
+    logged = True
+
+    owner = User.query.filter_by(email=session['email']).first()
+
     posts = []
     post_id = request.args.get('id')
 
     if post_id:
         posts = Posts.query.filter_by(id=post_id).all()
-        return render_template('blog-posts.html', posts=posts,
-        post_id = post_id)
+        return render_template('blog-posts.html',title = 'Blogz!', posts=posts,
+        post_id = post_id,logged=logged)
     else:
-        posts = Posts.query.all()
-        return render_template('blog-posts.html',posts = posts)
+        posts = Posts.query.filter_by(owner=owner).all()
+        return render_template('blog-posts.html',title = 'Blogz!',posts = posts,logged=logged,owner=owner)
     
 @app.route('/newpost', methods=['POST','GET'])
 def add_post():
+    logged = True
 
-    
     title_error = ""
     blog_post_error = ""
 
@@ -56,7 +109,8 @@ def add_post():
     if request.method == 'POST':
         blog_title = request.form['blog-title']
         blog_post = request.form['blog-post']
-        new_post = Posts(blog_title, blog_post)
+        owner = User.query.filter_by(email=session['email']).first()
+        new_post = Posts(blog_title, blog_post,owner)
 
         if len(blog_title) == 0:
             title_error = "Please enter a title"
@@ -70,7 +124,7 @@ def add_post():
             return redirect('/blog?id={0}'.format(new_post.id))
 
     return render_template('add-post.html', title_error = title_error,
-            blog_post_error = blog_post_error)
+            blog_post_error = blog_post_error, logged=logged)
 
 
 
